d11a171b09295ca7e21b9b0bbafc3ecb48b92dc7
admin <zacharyjsanders@gmail.com>
2019-09-12 19:03:44 -0600

    adding header component organizing directory structure

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { render } from "react-dom";
-import { Router, Link } from "@reach/router";
-import About from './components/about.js';
-import Home from './components/home.js';
+import Header from './components/header.js';
+
 
 
 
 const App = () => (
   <div>
-    <h1>Tutorial!</h1>
-    <nav>
-      <Link to="/">Home</Link>{" "}
-      <Link to="about">About</Link>
-    </nav>
+    <h1>Endurain Framework</h1>
+      <Header />
 
-    <Router>
-      <Home path="/" />
-      <About path="/about" />
-    </Router>
+
   </div>
 );
 
 
 
-export default App;
+export default App
---
+++ src/components/header.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Router, Link } from "@reach/router";
+import About from './about.js';
+import Home from './home.js';
+import Contact from './contact.js';
+import NotFound from './notfound.js';
+
+const Header = () => (
+
+  <header>
+    <nav>
+      <Link to="/">Home</Link>{" "}
+      <Link to="about">About</Link>
+      <Link to="contact">Contact</Link>
+    </nav>
+    <Router>
+      <Home path="/" />
+      <About path="/about" />
+      <Contact path="/contact" />
+      <NotFound default />
+    </Router>
+  </header>
+
+);
+
+export default Header
