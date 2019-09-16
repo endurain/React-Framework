57d4bf5579a8c349944930435a7995246803bcc8
admin <zacharyjsanders@gmail.com>
2019-09-16 17:25:17 -0600

    adding blogposts component

--- src/components/header.js
+++ src/components/header.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Router, Link } from "@reach/router";
 import About from './about.js';
 import Home from './home.js';
+import Blog from './blogposts.js';
 import Contact from './contact.js';
 import NotFound from './notfound.js';
 
@@ -11,11 +12,13 @@ const Header = () => (
     <nav>
       <Link to="/">Home</Link>{" "}
       <Link to="about">About</Link>
+      <Link to="blog">Blog</Link>
       <Link to="contact">Contact</Link>
     </nav>
     <Router>
       <Home path="/" />
       <About path="/about" />
+      <Blog path="/blog" />
       <Contact path="/contact" />
       <NotFound default />
     </Router>
